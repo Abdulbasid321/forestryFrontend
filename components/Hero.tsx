2cbcfba0685644e64448d79fae9e7246638f1885
abdulbasid <abdulbasidhussain9@gmail.com>
2025-09-23 16:25:05 -0700

    update

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -10,14 +10,12 @@ const Hero = () => {
         <h1 className="text-4xl md:text-5xl text-gray-900 mb-6 font-extrabold leading-tight">
           {/* Welcome to <span className="text-green-600">ZIT</span> <br /> */}
           <span>Empowering Learning Like Never Before</span><br />
-          <span>College of forestry Computer Science Department</span>
+          {/* <span>College of forestry Computer Science Department</span> */}
         </h1>
 
         <p className="text-base md:text-lg text-gray-600 mb-8">
           Explore a wide range of courses, connect with top instructors, and unlock your potential today.
         </p>
-        
-
         <div className="flex flex-col gap-4 md:flex-row justify-center md:justify-start">
           <button className="bg-green-500 text-white px-6 py-3 rounded-md hover:bg-green-600 transition">
             Get Started
